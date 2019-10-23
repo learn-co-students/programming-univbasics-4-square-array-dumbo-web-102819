04abc445ca11c5f4cb398dd800f92b77f86e4345
Ajani Queeley <ajaniqueeley@gmail.com>
2019-10-23 19:50:50 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,11 @@
 def square_array(array)
-  # your code here
+  counter = 0
+  while array[counter] do
+    array[counter].Math.sqrt
+    counter +=1
+  end
 end
+
+my_array = [5, 10]
+
+square_array(my_array)
